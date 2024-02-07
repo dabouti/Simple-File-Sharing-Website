1acae0970618ebd4edc77f2e90ddc810ba6a890b
andrewjf04 <a.feenstra@wustl.edu>
2024-02-07 01:30:13 -0600

    Update renamefile.php

--- renamefile.php
+++ renamefile.php
@@ -17,6 +17,11 @@ if (!preg_match('/^[\w_\.\-]+$/', $file)) {
     header("Location: listfiles.php");
     exit;
 }
+if (!preg_match('/^[\w_\.\-]+$/', $filerename)) {
+    $_SESSION['error'] = 'invalid filename';
+    header("Location: listfiles.php");
+    exit;
+}
 if (file_exists("/srv/protected/$username/$file")) {
     rename("/srv/protected/$username/$file", "/srv/protected/$username/$filerename");
     $_SESSION["success"] = "file renamed";
